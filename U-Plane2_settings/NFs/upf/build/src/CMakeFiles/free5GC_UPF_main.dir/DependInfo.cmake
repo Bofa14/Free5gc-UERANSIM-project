118a6b48084e3b222562528efe9338b8840070a8
Bofa14 <94922641+Bofa14@users.noreply.github.com>
2022-02-24 15:10:52 +0100

    U-Plane2

---
+++ U-Plane2_settings/NFs/upf/build/src/CMakeFiles/free5GC_UPF_main.dir/DependInfo.cmake
@@ -0,0 +1,46 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/ubuntu/free5gc/NFs/upf/src/n4/n4_dispatcher.c" "/home/ubuntu/free5gc/NFs/upf/build/src/CMakeFiles/free5GC_UPF_main.dir/n4/n4_dispatcher.c.o"
+  "/home/ubuntu/free5gc/NFs/upf/src/n4/n4_pfcp_build.c" "/home/ubuntu/free5gc/NFs/upf/build/src/CMakeFiles/free5GC_UPF_main.dir/n4/n4_pfcp_build.c.o"
+  "/home/ubuntu/free5gc/NFs/upf/src/n4/n4_pfcp_handler.c" "/home/ubuntu/free5gc/NFs/upf/build/src/CMakeFiles/free5GC_UPF_main.dir/n4/n4_pfcp_handler.c.o"
+  "/home/ubuntu/free5gc/NFs/upf/src/n4/n4_pfcp_path.c" "/home/ubuntu/free5gc/NFs/upf/build/src/CMakeFiles/free5GC_UPF_main.dir/n4/n4_pfcp_path.c.o"
+  "/home/ubuntu/free5gc/NFs/upf/src/up/up_match.c" "/home/ubuntu/free5gc/NFs/upf/build/src/CMakeFiles/free5GC_UPF_main.dir/up/up_match.c.o"
+  "/home/ubuntu/free5gc/NFs/upf/src/up/up_path.c" "/home/ubuntu/free5gc/NFs/upf/build/src/CMakeFiles/free5GC_UPF_main.dir/up/up_path.c.o"
+  "/home/ubuntu/free5gc/NFs/upf/src/upf.c" "/home/ubuntu/free5gc/NFs/upf/build/src/CMakeFiles/free5GC_UPF_main.dir/upf.c.o"
+  "/home/ubuntu/free5gc/NFs/upf/src/upf_config.c" "/home/ubuntu/free5gc/NFs/upf/build/src/CMakeFiles/free5GC_UPF_main.dir/upf_config.c.o"
+  "/home/ubuntu/free5gc/NFs/upf/src/upf_context.c" "/home/ubuntu/free5gc/NFs/upf/build/src/CMakeFiles/free5GC_UPF_main.dir/upf_context.c.o"
+  "/home/ubuntu/free5gc/NFs/upf/src/upf_init.c" "/home/ubuntu/free5gc/NFs/upf/build/src/CMakeFiles/free5GC_UPF_main.dir/upf_init.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "utlt_logger"
+  "../src"
+  "../lib/pfcp/include"
+  "../lib/utlt/include"
+  "../lib/utlt/logger/include"
+  "../updk/include"
+  "../updk/src/kernel/."
+  "updk/src/third_party/libgtp5gnl/include"
+  "../updk/src/kernel/knet/include"
+  "../updk/src/kernel/gtpv1/include"
+  "../updk/src/kernel/rule"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/ubuntu/free5gc/NFs/upf/build/updk/CMakeFiles/free5GC_updk.dir/DependInfo.cmake"
+  "/home/ubuntu/free5gc/NFs/upf/build/updk/src/kernel/CMakeFiles/free5GC_updk_kernel.dir/DependInfo.cmake"
+  "/home/ubuntu/free5gc/NFs/upf/build/lib/utlt/CMakeFiles/free5GC_utlt.dir/DependInfo.cmake"
+  "/home/ubuntu/free5gc/NFs/upf/build/lib/pfcp/CMakeFiles/free5GC_pfcp.dir/DependInfo.cmake"
+  "/home/ubuntu/free5gc/NFs/upf/build/updk/src/kernel/gtpv1/CMakeFiles/free5GC_gtpv1.dir/DependInfo.cmake"
+  "/home/ubuntu/free5gc/NFs/upf/build/updk/src/kernel/knet/CMakeFiles/free5GC_knet.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
